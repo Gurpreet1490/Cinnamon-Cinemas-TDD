81d354c61817fde146112f33bcdf7af889c0fcea
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-15 16:39:59 +0000

    code refactor in class

--- src/main/java/Model/SeatingPlan.java
+++ src/main/java/Model/SeatingPlan.java
@@ -1,6 +1,12 @@
 package Model;
 
 public class SeatingPlan {
+
+    public static final String ROW_SEQUENCE_ORDER = "ABCDEFGHIJKLMNOPQRSTUVW";
+
     public SeatingPlan(int maxRow, int rowSize) {
     }
+
+
+
 }
