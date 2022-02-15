d82a893d28dc6591c05ef852adb06ebdcbbb1f28
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-15 16:37:59 +0000

    Failing test for Row sequence

--- src/test/java/SeatingPlanTest.java
+++ src/test/java/SeatingPlanTest.java
@@ -1,13 +1,20 @@
 import Model.SeatingPlan;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 
 public class SeatingPlanTest {
 
     @Test
     public void practice(){
         new SeatingPlan(3, 5);
+    }
 
+    //Testing row sequence
+    @Test
+    public void stringTest(){
+        assertEquals("ABCDEFGHIJKLMNOPQRSTUVW", SeatingPlan.ROW_SEQUENCE_ORDER);
     }
 
 
